0639acb639c4b462289a3f30eb5cad6e53e337df
troyth <troyth@gmail.com>
2013-10-27 11:02:12 -0400

    max resolution

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ function onOpen() {
 		mode: "photo",
 		output: image_path + "image.png", // will change this before taking a picture
 		encoding: "png",
-		width: 1800,
-		height: 1200,
+		width: 2592,
+		height: 1944,
 		timeout: 0 // take snapshot immediately with 0 delay
 	});
 
